578023037a59633ef55ec52a1fdfdabfce362d64
isaacp.official@gmail.com <58235609+iamisaacp@users.noreply.github.com>
2022-10-05 16:56:20 +0100

    ADded port forwarding

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ad41313e3f7c645aea6f566398a6496b-1236860623.us-east-1.elb.amazonaws.com'
 };
 
 /*

59e47ee3e691823a9f70ac8361c3205583219090
isaacp.official@gmail.com <58235609+iamisaacp@users.noreply.github.com>
2022-10-06 02:00:55 +0100

    Added Port FOrwadign

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af7e4f8832a2845d9998dcaf464b79f5-1553088267.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
